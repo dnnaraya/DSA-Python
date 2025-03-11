21e1de2594029888b31ca9f05c2d01a4efe71387
dnnaraya <dhivyanarayanan07@gmail.com>
2025-03-11 11:12:49 +0530

    Added Problem 4 - Determine if string has all unique characters

--- DSA_Problems/Arrays_String.py
+++ DSA_Problems/Arrays_String.py
@@ -28,39 +28,39 @@ def is_valid_ip(str_ip: str) -> bool:
 def validate_ip_address(self, queryIP: str) -> str:
     result = ['IPv4', 'IPv6', 'Neither']
 
-        if '.' in queryIP and len(queryIP.split('.')) == 4:
+    if '.' in queryIP and len(queryIP.split('.')) == 4:
 
-            arr_IP = queryIP.split('.')
-            for i in arr_IP:
-                try:
-                    cur = int(i)
-                    if cur != 0 and len(i) - len(i.lstrip('0')) != 0:
-                        return 'Neither'
-                    elif (cur < 0) or (cur > 255):
-                        return 'Neither'
-
-                except ValueError:
+        arr_IP = queryIP.split('.')
+        for i in arr_IP:
+            try:
+                cur = int(i)
+                if cur != 0 and len(i) - len(i.lstrip('0')) != 0:
+                    return 'Neither'
+                elif (cur < 0) or (cur > 255):
                     return 'Neither'
 
-            return 'IPv4'
+            except ValueError:
+                return 'Neither'
 
-        elif ':' in queryIP and len(queryIP.split(':')) == 8:
+        return 'IPv4'
 
-            arr_IP6 = queryIP.split(":")
-            valid_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B',
-                           'C', 'D', 'E', 'F']
-            for i in arr_IP6:
-                if len(i) >= 1 and len(i) <= 4:
-                    for x in i:
-                        if x not in valid_chars:
-                            return 'Neither'
-                else:
-                    return 'Neither'
+    elif ':' in queryIP and len(queryIP.split(':')) == 8:
 
-            return 'IPv6'
+        arr_IP6 = queryIP.split(":")
+        valid_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B',
+                       'C', 'D', 'E', 'F']
+        for i in arr_IP6:
+            if len(i) >= 1 and len(i) <= 4:
+                for x in i:
+                    if x not in valid_chars:
+                        return 'Neither'
+            else:
+                return 'Neither'
 
-        else:
-            return 'Neither'
+        return 'IPv6'
+
+    else:
+        return 'Neither'
 
 # problem 3: Reverse Words in a String - Solution 1
 def reverseWords1(self, s: str) -> str:
@@ -75,6 +75,7 @@ def reverseWords1(self, s: str) -> str:
                 reverse_word += " "
     return reverse_word
 
+# Solution 2
 def reverseWords2(self, s: str) -> str:
     words = s.split()
 
@@ -89,6 +90,18 @@ def reverseWords2(self, s: str) -> str:
     return " ".join(words)
 
 
+# Problem 4 : Determine if string has all unique characters
+def has_unique_chars(s: str) -> bool:
+
+    for i in s:
+        if s.count(i) > 1:
+            return False
+
+    return True
+
+
+
+
 arr: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(even_sum(arr))
 
@@ -96,3 +109,8 @@ queryIP1 = "172.16.254.1"
 queryIP2 = "172.16.@4.256"
 print("Is this IP ", queryIP1, "valid? : ", is_valid_ip(queryIP1))
 print("Is this IP ", queryIP2, "valid? : ", is_valid_ip(queryIP2))
+
+str1 = "abcdefgh"
+str2 = "aabbjhjgfg"
+print("Is the string ", str1, "has unique characters? :", has_unique_chars(str1))
+print("Is the string ", str2, "has unique characters? :", has_unique_chars(str2))
